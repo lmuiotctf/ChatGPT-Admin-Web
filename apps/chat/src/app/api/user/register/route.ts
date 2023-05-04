698020088c1c6e545b0825ea2824831872b04e4a
lmuiotctf <52068443+lmuiotctf@users.noreply.github.com>
2023-05-04 17:16:16 +0800

    Update route.ts

--- apps/chat/src/app/api/user/register/route.ts
+++ apps/chat/src/app/api/user/register/route.ts
@@ -9,8 +9,6 @@ import {
 import { sendEmail } from "@/lib/email";
 import { ReturnStatus, ResponseStatus } from "@/app/api/typing.d";
 const ifVerifyCode = !!process.env.EMAIL_DOMAIN;
-
-
 export async function POST(req: NextRequest): Promise<Response> {
   try {
     const { email, password, code, code_type, phone, invitation_code } =
